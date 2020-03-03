b79a1114d66a5ea79a91d1ad4830418f7c5bd8f7
Bowgo Tsai <bowgotsai@google.com>
2020-03-03 12:15:50 +0800

    sm8550-common: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          $OUT/recovery/root/first_stage_ramdisk/avb/
    Change-Id: I8a002ba6f1421fb460056ccae6572050bdb0ce3c

--- common.mk
+++ common.mk
@@ -17,9 +17,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
