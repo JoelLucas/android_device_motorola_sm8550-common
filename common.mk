5e5bcd4a57af78df02540dc097bef7f465c2b6f7
Bruno Martins <bgcngm@gmail.com>
2024-03-11 17:23:49 +0100

    sm8550-common: Migrate to common libqti-perfd-client and power-libperfmgr
    Change-Id: I22dd0baaf1594db1650bccf37b4d167b50135174

--- common.mk
+++ common.mk
@@ -291,7 +291,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/kalama/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
@@ -317,7 +318,11 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/motorola
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/motorola \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
